6601889911c68c28ff63c90d16f273b217a94495
Michael Chen <michaelchen298@gmail.com>
2026-09-24 15:54:44 -0500

    Update Model.java

--- Model.java
+++ Model.java
@@ -11,6 +11,10 @@ public class Model {
         // Implement the logic to update the model state
     }
 
+    public boolean updatePlayer(int id, String newCodename) {
+        return playerDatabase.updatePlayer(id, newCodename);
+    }
+
     public PlayerDatabase getPlayerDatabase() {
         return playerDatabase;
     }
